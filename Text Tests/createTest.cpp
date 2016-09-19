042f37d98f2ff84958aba5483376a403360506b0
nfs13a <nfs13a@acu.edu>
2016-09-18 20:41:55 -0500

    Nevan's text test
    created a test text file by making a .cpp that generates test files from
    command line
    added prereq data to feature file

---
+++ Text Tests/createTest.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+int main() {
+	string filename;
+	cin >> filename;
+	//string prefixes[] = {"Math", "CS", "IT", "ACCT", "BUSA", "MGMT"};
+	//string suffixes[] = {"101", "110", "111", "120", "210", "211", "301", "304", "310", "324"};
+
+	vector<string> prefixes;
+	prefixes.push_back("Math");
+	prefixes.push_back("CS");
+	prefixes.push_back("IT");
+	prefixes.push_back("ACCT");
+	prefixes.push_back("BUSA");
+	prefixes.push_back("MGMT");
+
+	vector<string> suffixes;
+	suffixes.push_back("101");
+	suffixes.push_back("110");
+	suffixes.push_back("111");
+	suffixes.push_back("120");
+	suffixes.push_back("210");
+	suffixes.push_back("211");
+	suffixes.push_back("301");
+	suffixes.push_back("304");
+	suffixes.push_back("310");
+	suffixes.push_back("324");
+
+	ofstream fout;
+	fout.open(filename.c_str());
+	
+	for (int i = 10902; i <= 10903; i++) {
+		//CRN
+		fout << i << ",";
+		cout << "CRN" << endl;
+
+		//make 10 students
+		for (int j = 0; j < 10; j++) {
+			//banner id
+			fout << "000";
+			for (int k = 0; k < 6; k++)
+				fout << rand() % 9;
+			fout << ",";
+			cout << "banner id" << endl;
+
+			//classes		
+			for (int k = rand() % 5 + 0; k < 10; k++) {
+				//fout << prefixes[rand() % 5] << "," << suffixes[rand() % 9];
+				fout << prefixes.at(rand() % 5) << suffixes.at(rand() % 9) << ",";
+				char c = rand() % 6 + 65;
+				while (c == 'E')
+					c = rand() % 6 + 65;
+				fout << c << ",";
+			}
+			cout << "class list" << endl;
+		}
+
+		fout << "\n";
+	}
+  	fout.close();
+}
--- cucumber/features/Eject.feature
+++ cucumber/features/Eject.feature
@@ -3,7 +3,9 @@ Feature: people take a class
 #he seems to only ever teaach these two classes, but could be more
 #sometimes teaches more than one section
 
-#for fall 2016: 211 - 10902; 324 - 10903
+#for fall 2016: 
+	#211 - 10902 Prerequisite: ACCT 210 with a grade of "C" or better.
+	#324 - 10903 Prerequisites: ACCT 210, BUSA 120, both with grade of "C" or better.
 
 	Scenario: Dr. Clements enters ACCT 211 's CRN
 
