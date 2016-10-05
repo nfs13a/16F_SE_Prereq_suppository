29c7ab7dc128b54397c5a97e3b9a772b0718f440
geigan <geiganax@gmail.com>
2016-10-05 16:18:39 -0500

    Getting rid of num, and adding prereqCourse table

--- implementation/StudentsSetup.sql
+++ implementation/StudentsSetup.sql
@@ -4,6 +4,7 @@ USE STUDENTS;
 SET foreign_key_checks = 0;
 DROP TABLE IF EXISTS student;
 DROP TABLE IF EXISTS course;
+DROP TABLE IF EXISTS prereqCourse;
 DROP TABLE IF EXISTS courseInstances;
 DROP TABLE IF EXISTS studentCoursesTaken;
 SET foreign_key_checks = 1;
@@ -20,31 +21,37 @@ PRIMARY KEY ( banner )
 
 CREATE TABLE course
 ( code varchar(20) NOT NULL,
-num varchar(20) NOT NULL,
 hours varchar(50) NOT NULL,
-PRIMARY KEY (code, num)
+prereqGPA float(5) NOT NULL,
+prereqClass varchar(50) NOT NULL,
+PRIMARY KEY ( code, num)
+);
+
+CREATE TABLE prereqCourse
+( codePre varchar(20) NOT NULL,
+codePost varchar(20) NOT NULL,
+grade varchar(10) NOT NULL,
+PRIMARY KEY (codePre,codePost),
+FOREIGN KEY (codePre) references course(code),
+FOREIGN KEY (codePost) references course(code)
 );
 
 CREATE TABLE courseInstances
 ( CRN varchar(50) NOT NULL,
 code varchar(20) NOT NULL,
-num varchar(20) NOT NULL,
 instructor varchar(50) NOT NULL,
-PRIMARY KEY (CRN,code,num),
-FOREIGN KEY (code,num) references course(code,num)
-#FOREIGN KEY (num) references course(num)
+PRIMARY KEY (CRN,code),
+FOREIGN KEY (code) references course(code)
 );
 
 CREATE TABLE studentCoursesTaken
 ( banner varchar(20) NOT NULL,
 CRN varchar(50) NOT NULL,
 code varchar(20) NOT NULL,
-num varchar(20) NOT NULL,
 earnedHrs int NOT NULL,
 grade varchar(10) NOT NULL,
-PRIMARY KEY (CRN,code,num,banner),
-FOREIGN KEY (code,num) references course(code,num),
-#FOREIGN KEY (num) references course(num),
+PRIMARY KEY (CRN,code,banner),
+FOREIGN KEY (code) references course(code),
 FOREIGN KEY (CRN) references courseInstances(CRN),
 FOREIGN KEY (banner) references student(banner)
 );
