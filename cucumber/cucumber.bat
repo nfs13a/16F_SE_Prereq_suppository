4260dc175f4d2769d9eb4570ee4cf09f7423620e
nfs13a <nfs13a@acu.edu>
2016-10-05 18:08:18 -0500

    changed a -f to a -p in cucumber.bat

--- cucumber/cucumber.bat
+++ cucumber/cucumber.bat
@@ -3,4 +3,4 @@ rem how do we make this box agnostic if we have to deal with "jars/*:." vs "jars
 rem java -cp "jars/*;." cucumber.api.cli.Main -p pretty features -g step_definitions
 
 javac -cp "jars/*;." step_definitions/EjectionTest.java implementation/Catalogue.java implementation/Transcript.java implementation/Roster.java implementation/Registrar.java
-java -cp "jars/*;." cucumber.api.cli.Main -f progress --snippets camelcase -g step_definitions features
+java -cp "jars/*;." cucumber.api.cli.Main -p progress --snippets camelcase -g step_definitions features
